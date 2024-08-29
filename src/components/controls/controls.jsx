2385ba5771460fe68818b6416addc04a8fe9d31d
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-08-29 09:36:52 +0200

    svg masks for player 1 and AI , changes only done in controls component

--- src/components/controls/controls.jsx
+++ src/components/controls/controls.jsx
@@ -1,10 +1,19 @@
 import styles from './controls.module.scss';
+import FlatMaskIcon from '../../assets/images/flat-mask.svg';
+import StripedMaskIcon from '../../assets/images/striped-mask.svg';
+import { useEffect } from 'react';
 
 export const Controls = ({ onRollDice, currentPlayer, rolling}) => {
 
+    useEffect(() => {
+        console.log(`It's ${currentPlayer}'s turn`);  
+    }, [currentPlayer]);
+
     return(
         <div className={styles.controls}>
-            <p>{currentPlayer}'s Turn</p>
+            <h2>{currentPlayer}'s Turn</h2>
+            {currentPlayer === 'Player 1' && <img src={FlatMaskIcon} alt="Player 1 Mask"/>}
+            {currentPlayer === 'AI' && <img src={StripedMaskIcon} alt="AI Mask"/>}
             <button onClick={onRollDice} disabled={rolling}>
                 {rolling ? 'Rolling...' : 'Roll Dice'}
             </button>
