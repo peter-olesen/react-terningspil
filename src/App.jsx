cec642cd81fd6d69fa010322bc5355b747085294
Gunski <gunnarkryth@gmail.com>
2024-08-29 09:24:43 +0200

    player 1 colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+  <>
+  
+  </>
+  );
 }
 
-export default App
+export default App;
